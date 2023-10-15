612bd3d7e0b15ccffb25ed6042414041c236cbcd
renancatan <renancatan@gmail.com>
2023-10-15 01:53:18 -0300

    Initial commit for portfolio

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var acc = document.getElementsByClassName("accordion-button");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
+});
